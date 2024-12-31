c7ad840504dab6e2fa1327b31436a23d1b293829
Carlos Caño <canogomezcarlos@gmail.com>
2024-12-31 17:22:30 +0100

    Initial Commit

---
+++ 2_Curso_iOS/__Curso_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  __Curso_iOSApp.swift
+//  2_Curso_iOS
+//
+//  Created by Carlos C on 31/12/24.
+//
+
+import SwiftUI
+
+@main
+struct __Curso_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
