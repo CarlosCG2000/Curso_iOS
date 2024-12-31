572a2a8862b77db2cc291426f1e9b0fbbec910cb
Carlos Caño <canogomezcarlos@gmail.com>
2024-12-31 13:50:41 +0100

    primeros pasos

--- 02_CursoiOS/ContentView.swift
+++ 02_CursoiOS/ContentView.swift
@@ -9,21 +9,23 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
+        VStack { // Vista 1
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
             Text("Hello, world!")
+            Text("Hello, world!").bold().background(Color.green).foregroundColor(.pink)
             
             Button( action: { print("Pulsado el botón") },
                     label: { Text("Boton") }
             )
-                
-            }
+            
+        }
         .padding()
     }
 }
 
+// Para visualizar la vista
 #Preview {
     ContentView()
 }
